402e59cabe0be7aaaf27a0d5cebffc7e03430b64
Scott Wofford <skawofford@gmail.com>
2024-08-16 19:23:17 -0400

    Task 1 step 2

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -268,7 +268,21 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
+            {plantsArray.map((category, index) => (
+            <div key={index}>
+              <h1><div>{category.category}</div></h1>
+              <div className='product-list'>
+                {category.plants.map((plant, plantIndex) => (
+                <div className='product-card' key={plantIndex}>
+                  <img className='product-image' src={plant.image} alt={plant.description} />
+                  <div className='product-title'>{plant.name}</div>
+                  <div className='product-description'>{plant.description}</div>
+                  <div className='product-cost'>{plant.cost}</div>
+                </div>
+              ))}
+              </div>
+            </div>
+            ))}
 
         </div>
  ) :  (
